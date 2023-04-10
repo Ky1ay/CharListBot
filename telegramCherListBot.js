d563b0f2d251d438955acdebec83aa5a9ffdc066
Ivan Kulaev <kylayivan@gmai.com>
2023-04-10 23:51:37 +0300

    add button "Back"

--- telegramCherListBot.js
+++ telegramCherListBot.js
@@ -19,17 +19,69 @@ bot.onText(/\/start/, (msg) => {
   });
 });
 
-bot.onText(/Дополнительные действия/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Выберите одно из дополнительных действий", {
-    reply_markup: {
-      keyboard: [
-        [{ text: "Описание персонажа" }, { text: "Действие 2" }]
-      ],
-      resize_keyboard: true,
-      one_time_keyboard: true
-    }
-  });
+bot.onText(/Дополнительные действия|Назад/, (msg) => {
+  if (msg.text === 'Назад') {
+    bot.sendMessage(msg.chat.id, "Выберите действие", {
+      reply_markup: {
+        keyboard: [
+          [{ text: "Дополнительные действия" }]
+        ],
+        resize_keyboard: true,
+        one_time_keyboard: true
+      }
+    });
+  } else {
+    bot.sendMessage(msg.chat.id, "Выберите одно из дополнительных действий", {
+      reply_markup: {
+        keyboard: [
+          [{ text: "Описание персонажа" }, { text: "Действие 2" }, { text: "Назад" }]
+        ],
+        resize_keyboard: true,
+        one_time_keyboard: true
+      }
+    });
+  }
 });
+
+bot.on('callback_query', (callbackQuery) => {
+  const action = callbackQuery.data;
+
+  if (action === 'editName' || action === 'editRace') {
+    const fieldName = action === 'editName' ? 'name' : 'race';
+
+    bot.answerCallbackQuery(callbackQuery.id);
+
+    bot.sendMessage(callbackQuery.message.chat.id, `Введите новое значение для ${fieldName}`);
+
+    bot.once('message', (newDataMessage) => {
+      const characterData = JSON.parse(fs.readFileSync(characterDataFilePath, 'utf8'));
+      characterData[fieldName] = newDataMessage.text;
+
+      fs.writeFileSync(characterDataFilePath, JSON.stringify(characterData), 'utf8');
+
+      bot.sendMessage(callbackQuery.message.chat.id, `Успешно обновлено`);
+
+      const inlineKeyboard = [
+        [
+          { text: 'Редактировать имя', callback_data: 'editName' },
+          { text: 'Редактировать расу', callback_data: 'editRace' },
+        ],
+      ];
+
+      bot.sendMessage(callbackQuery.message.chat.id, `Имя: ${characterData.name}\nРаса: ${characterData.race}`, {
+        reply_markup: {
+          inline_keyboard: inlineKeyboard,
+        },
+      });
+
+    });
+  }
+});
+
+bot.onText(/Действие 2/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Это действие ещё в разработке");
+});
+
 
 bot.onText(/Описание персонажа/, (msg) => {
   const characterData = JSON.parse(fs.readFileSync(characterDataFilePath, 'utf8'));
@@ -46,34 +98,4 @@ bot.onText(/Описание персонажа/, (msg) => {
       inline_keyboard: inlineKeyboard,
     },
   });
-});
-
-bot.on('callback_query', (callbackQuery) => {
-  const action = callbackQuery.data;
-
-  if (action === 'editName' || action === 'editRace') {
-    const fieldName = action === 'editName' ? 'name' : 'race';
-
-    bot.answerCallbackQuery(callbackQuery.id);
-
-    bot.sendMessage(callbackQuery.message.chat.id, `Введите новое значение для "${fieldName}"`);
-
-    bot.once('message', (newDataMessage) => {
-      const characterData = JSON.parse(fs.readFileSync(characterDataFilePath, 'utf8'));
-      characterData[fieldName] = newDataMessage.text;
-
-      fs.writeFileSync(characterDataFilePath, JSON.stringify(characterData), 'utf8');
-
-      bot.sendMessage(callbackQuery.message.chat.id, `"${fieldName}" успешно обновлено на "${newDataMessage.text}"`, {
-        reply_markup: {
-          inline_keyboard: [
-            [
-              { text: 'Редактировать имя', callback_data: 'editName' },
-              { text: 'Редактировать расу', callback_data: 'editRace' },
-            ],
-          ],
-        },
-      });
-    });
-  }
 });
