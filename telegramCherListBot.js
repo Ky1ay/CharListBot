fb6bf0d85e6b299e0b17f4b1d0cfab1495591b32
Ivan Kulaev <kylayivan@gmai.com>
2023-04-10 23:32:04 +0300

    fix

--- telegramCherListBot.js
+++ telegramCherListBot.js
@@ -1,13 +1,12 @@
 const TelegramBot = require('node-telegram-bot-api');
-const description = require('./D&D/MainBlock/characterDescription');
+const fs = require('fs');
+const path = require('path');
 
-// Токен вашего бота
-const token = '5911525581:AAGR8QB3g-YCV7Ca1PNtcdazcMHOxwo_O20';
-
-// Создаем экземпляр бота
+const token = '6021086571:AAHSmcPEo9TY51ru60wQE89yClu03sowJYs';
 const bot = new TelegramBot(token, { polling: true });
 
-// Обработчик команды /start
+const characterDataFilePath = path.join(__dirname, 'D&D/MainBlock/charDescrip_data.json');
+
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "Выберите действие", {
     reply_markup: {
@@ -20,7 +19,6 @@ bot.onText(/\/start/, (msg) => {
   });
 });
 
-// Обработчик кнопки "Дополнительные действия"
 bot.onText(/Дополнительные действия/, (msg) => {
   bot.sendMessage(msg.chat.id, "Выберите одно из дополнительных действий", {
     reply_markup: {
@@ -33,8 +31,49 @@ bot.onText(/Дополнительные действия/, (msg) => {
   });
 });
 
-// Обработчик кнопки "Описание персонажа"
 bot.onText(/Описание персонажа/, (msg) => {
-  description.handleDescription(bot, msg);
+  const characterData = JSON.parse(fs.readFileSync(characterDataFilePath, 'utf8'));
+
+  const inlineKeyboard = [
+    [
+      { text: 'Редактировать имя', callback_data: 'editName' },
+      { text: 'Редактировать расу', callback_data: 'editRace' },
+    ],
+  ];
+
+  bot.sendMessage(msg.chat.id, `Имя: ${characterData.name}\nРаса: ${characterData.race}`, {
+    reply_markup: {
+      inline_keyboard: inlineKeyboard,
+    },
+  });
 });
 
+bot.on('callback_query', (callbackQuery) => {
+  const action = callbackQuery.data;
+
+  if (action === 'editName' || action === 'editRace') {
+    const fieldName = action === 'editName' ? 'name' : 'race';
+
+    bot.answerCallbackQuery(callbackQuery.id);
+
+    bot.sendMessage(callbackQuery.message.chat.id, `Введите новое значение для "${fieldName}"`);
+
+    bot.once('message', (newDataMessage) => {
+      const characterData = JSON.parse(fs.readFileSync(characterDataFilePath, 'utf8'));
+      characterData[fieldName] = newDataMessage.text;
+
+      fs.writeFileSync(characterDataFilePath, JSON.stringify(characterData), 'utf8');
+
+      bot.sendMessage(callbackQuery.message.chat.id, `"${fieldName}" успешно обновлено на "${newDataMessage.text}"`, {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              { text: 'Редактировать имя', callback_data: 'editName' },
+              { text: 'Редактировать расу', callback_data: 'editRace' },
+            ],
+          ],
+        },
+      });
+    });
+  }
+});
